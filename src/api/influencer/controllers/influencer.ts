99902432a963324c5e45bc07e84b5317eeacf0be
Gregory Ho <87746215+monsieurho@users.noreply.github.com>
2025-01-14 23:09:18 +0800

    Update environment configuration and refactor plugin settings for improved security and clarity

---
+++ src/api/influencer/controllers/influencer.ts
@@ -0,0 +1,7 @@
+/**
+ * influencer controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::influencer.influencer');
---
+++ src/api/influencer/routes/influencer.ts
@@ -0,0 +1,7 @@
+/**
+ * influencer router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::influencer.influencer');
